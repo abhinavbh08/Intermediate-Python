40ab367be31662e6a2973b7d4b18c2a3b30c53a8
abhinavbh08 <abhinavbhatt.999@gmail.com>
2020-01-13 23:36:30 +0530

    Completed argparse tutorial

--- argarse_cli.py
+++ argarse_cli.py
@@ -1,12 +1,28 @@
-def calc(x, y, operation):
-	if operation == "add":
-		return x + y
-	if operation == "sub":
-		return x - y
-	if operation == "mul":
-		return x * y
-	if operation == "div":
-		return x / y
+import argparse
+import sys
 
-operation = calc(7, 3, "div")
-print(operation)
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--x", type=float, default=1.0,
+		help="What is the first number?")
+	parser.add_argument("--y", type=float, default=1.0,
+		help="What is the second number?")
+	parser.add_argument("--operation", type=str, default="add",
+		help="What operation? (add, mul, sub, div)")
+	args = parser.parse_args()
+	print(calc(args))
+
+def calc(args):
+	if args.operation == "add":
+		return args.x + args.y
+	if args.operation == "sub":
+		return args.x - args.y
+	if args.operation == "mul":
+		return args.x * args.y
+	if args.operation == "div":
+		return args.x / args.y
+
+# Use this file in the form of python argparse_cli.py --x=2 --y=8 --operation=mul
+
+if __name__ == "__main__":
+	main()
